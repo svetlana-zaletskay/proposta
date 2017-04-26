2cb425b24fb035d7890afd72a6e9cd2732961ac6
svetlana-zaletskay <svetka-pipetka10@mail.ru>
2017-04-26 10:41:09 +0300

    LAST COMMIT

---
+++ js/proposta.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#slickProposta').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+    });
+});
+
+
+
+
+
+
